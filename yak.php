d2c0c477647fb02c42a02a3f5f344678baf5b35f
aami_sol <aamirmalliik@gmail.com>
2019-01-13 22:12:55 +0500

    table through loops

--- yak.php
+++ yak.php
@@ -1,13 +1,32 @@
-<<!DOCTYPE html>
+<!DOCTYPE html>
 <html>
 <head>
     <title>Yak Trips</title>
 </head>
 <body>
+    <table align="center" border="1" cellpadding="18" cellspacing="0" bgcolor="#d8c8ff">
     <?php
-    echo "Yak Tours and Trips";
-    ?>
-    <?php echo "All tours are availeble"; ?>
-    
+        for($i=1;$i<=4;$i++)
+        {
+        //echo "<tr>";
+        echo "<td>Table ".($i+1)."</td>";
+        //echo "</tr>";
+        }
+        ?>
+    </table>
+
+    <table align="center" border="1" cellpadding="7" cellspacing="0" bgcolor="#d4d4d4">
+        <?php
+        for($i=1;$i<=10;$i++)
+        {
+        echo "<tr>";
+        for ($j=2;$j<=5;$j++)
+        {
+        echo "<td>$j *$i = ".$i*$j."</td>";
+        }
+        echo "</tr>";
+        }
+        ?>
+    </table>
 </body>
 </html>
